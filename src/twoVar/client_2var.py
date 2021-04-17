95080903e6191f04d5986d4e7fa7acc29b6572b0
deboralagemb <deboralagemb@gmail.com>
2021-04-16 22:11:40 -0300

    accepting connection, will work on other bugs

--- src/twoVar/client_2var.py
+++ src/twoVar/client_2var.py
@@ -15,6 +15,72 @@ def port_in_use(port, obj):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex((obj._host, int(port))) == 0
 
+class VariableContext(object):
+    def __init__(self, client, var_name, queue, okr, requested):
+        self.client = client
+        self.var_name = var_name
+        self.queue = queue
+        self.okr = okr
+        self.requested = requested
+
+    def handle_msg_okr(self):
+        self.requested = False
+        self.okr = True
+
+    def handle_update_queue(self, msg):
+        if len(msg) > 0:
+            if msg[0] == '%pop%':
+                self.queue.pop(0)
+                print('\n[%s]: Queue atualizada: ação release %s' % (self.client.name, self.queue))
+            elif msg[0] == '%app%':  # Atualização na queue (próximo acquire recebido).
+                self.queue.append(msg[1])
+                print('\n[%s]: Queue atualizada: ação acquire %s' % (self.client.name, self.queue))
+            else:
+                self.deal_with_queue(msg)
+        else:
+            self.deal_with_queue(msg)
+
+    def deal_with_queue(self, msg):
+        # mensagem aqui: ['-var-X', ['Débora'], '-var-Y', []]
+        print('\n[%s]: Queue atualizada: ' % self.client.name, end='')
+        if self.queue == None:  # Subscribe.
+            print('ação subscribe %s' % self.queue)
+        else:
+            print('sincronizando contexto com o broker backup')
+            # notify() também entraria aqui
+
+        # todo pegar a queue apenas na posição referente da msg
+        self.queue = msg
+
+        self.okr = True  # Caso seja sincronização com o backup e o cliente tenha mandado mensagem de release para o principal não lida.
+        if self.client.name in self.queue:
+            self.requested = True
+
+    def handle_use_variable(self):
+        if not self.requested:  # Se já não mandou um 'acquire'.
+            aleatorio = random.uniform(0.5, 2)
+            time.sleep(aleatorio)
+            print('tentou requisição')
+            self.client.try_connection(self.client.name + ' -acquire %s %s %s' % (self.var_name, self.client._host, self.client._port))
+            print('requisitou')
+        elif self.queue != None and self.okr:  # Já deu subscribe.
+            time.sleep(
+                0.5)  # Pode ter casos em que o broker vá receber acquire corretamente mais levará mais que 0.5s para responder, o que irá gerar acquire duplo.
+            if self.client.name not in self.queue:  # Tratando caso em que broker principal cai logo após receber acquire.
+                # print('\n(!) ---> [%s] My acquire request somehow was never received ... 🤔 trying again!' % self.name)
+                self.requested = False
+                # todo: coloquei elif e apaguei o continue
+
+            elif len(self.queue) > 0:
+                proximo = self.queue[0]  # Ex.: ['Débora', '-acquire', '-var-X']
+                if proximo == self.client.name:
+                    time.sleep(random.uniform(0.2, 0.5))  # Faça algo com var-X
+
+                    self.client.try_connection(self.client.name + ' -release %s ' + self.client._host + ' ' + str(self.client._port) % (self.var_name))
+                    # print('%s liberou o recurso' % self.name)
+                    with self.client._lock:
+                        self.okr = False
+
 # cada variável tem sua queue
 # seu okr
 # seu requested
@@ -102,9 +168,15 @@ class Client:
         # print("Entering request thread as %s" % self.name)
 
         while not event.is_set() and not self.terminate:
+            print('aa: ' + str(not self.variablesContext))
             # todo thread para esse caso
+            if not self.variablesContext:
+                testing = VariableContext(self, '-var-X', None, True, False)
+                self.variablesContext.append(testing)
+
             for var in self.variablesContext:
-                self.variablesContext[var].handle_use_variable()
+                print('entrou no for var: ', var)
+                var.handle_use_variable()
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print("[%s] Closing request thread." % self.name)
@@ -143,12 +215,10 @@ class Client:
             time.sleep(duracao)  # Tempo da aplicação.
             event.set()
 
-
 with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    executor.submit(Client('Débora', '127.0.0.1', 8081).start)
+    executor.submit(Client('Debora', '127.0.0.1', 8081).start)
     # executor.submit(Client('Felipe', '127.0.0.1', 8082).start)
     # executor.submit(Client('Gabriel', '127.0.0.1', 8083).start)
-
 
 # Util.
 
@@ -157,68 +227,3 @@ def port(port):
         return True
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', int(port))) == 0
-
-class VariableContext:
-    def __init__(self, client, var_name, queue, okr, requested):
-        self.client = Client(client)
-        self.var_name = var_name
-        self.queue = queue
-        self.okr = okr
-        self.requested = requested
-
-    def handle_msg_okr(self):
-        self.requested = False
-        self.okr = True
-
-    def handle_update_queue(self, msg):
-        if len(msg) > 0:
-            if msg[0] == '%pop%':
-                self.queue.pop(0)
-                print('\n[%s]: Queue atualizada: ação release %s' % (self.client.name, self.queue))
-            elif msg[0] == '%app%':  # Atualização na queue (próximo acquire recebido).
-                self.queue.append(msg[1])
-                print('\n[%s]: Queue atualizada: ação acquire %s' % (self.client.name, self.queue))
-            else:
-                self.deal_with_queue(msg)
-        else:
-            self.deal_with_queue(msg)
-
-    def deal_with_queue(self, msg):
-        # mensagem aqui: ['-var-X', ['Débora'], '-var-Y', []]
-        print('\n[%s]: Queue atualizada: ' % self.client.name, end='')
-        if self.queue == None:  # Subscribe.
-            print('ação subscribe %s' % self.queue)
-        else:
-            print('sincronizando contexto com o broker backup')
-            # notify() também entraria aqui
-
-        # todo pegar a queue apenas na posição referente da msg
-        self.queue = msg
-
-        self.okr = True  # Caso seja sincronização com o backup e o cliente tenha mandado mensagem de release para o principal não lida.
-        if self.client.name in self.queue:
-            self.requested = True
-
-    def handle_use_variable(self):
-        if not self.requested:  # Se já não mandou um 'acquire'.
-            aleatorio = random.uniform(0.5, 2)
-            time.sleep(aleatorio)
-            self.client.try_connection(self.client.name + ' -acquire %s %s %s' % (self.var_name, self.client._host, self.client._port))
-
-        elif self.queue != None and self.okr:  # Já deu subscribe.
-            time.sleep(
-                0.5)  # Pode ter casos em que o broker vá receber acquire corretamente mais levará mais que 0.5s para responder, o que irá gerar acquire duplo.
-            if self.client.name not in self.queue:  # Tratando caso em que broker principal cai logo após receber acquire.
-                # print('\n(!) ---> [%s] My acquire request somehow was never received ... 🤔 trying again!' % self.name)
-                self.requested = False
-                # todo: coloquei elif e apaguei o continue
-
-            elif len(self.queue) > 0:
-                proximo = self.queue[0]  # Ex.: ['Débora', '-acquire', '-var-X']
-                if proximo == self.client.name:
-                    time.sleep(random.uniform(0.2, 0.5))  # Faça algo com var-X
-
-                    self.client.try_connection(self.client.name + ' -release %s ' + self.client._host + ' ' + str(self.client._port) % (self.var_name))
-                    # print('%s liberou o recurso' % self.name)
-                    with self.client._lock:
-                        self.okr = False
